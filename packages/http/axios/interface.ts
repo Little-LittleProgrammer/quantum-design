908916ab4b226b44074e1382bedecbe29515d83a
EvanWu <850123119@qq.com>
2024-04-08 17:57:43 +0800

    fix: 修改文件名

---
+++ packages/http/axios/interface.ts
@@ -0,0 +1,29 @@
+
+export interface RequestOptions {
+    // 是否返回原生的response
+    isReturnNativeResponse?: boolean;
+    // 接口前缀
+    joinPrefix?: boolean;
+    // 接口地址
+    apiUrl?: string;
+    urlPrefix?: string;
+    // 错误提示方式 default: 'message'
+    errorMessageCb?: (errCode: number, errMes: string) => void;
+    // 是否携带时间戳
+    joinTime?: boolean;
+    cancelToken?: boolean;
+    joinCookie?:boolean;
+    uploadUrl?:string;
+    env?: () =>string;
+    [key: string]: any
+    // 是否在header中携带token
+    withToken?: boolean;
+    // 请求重试机制, 暂未实现
+    retryRequest?: RetryRequest;
+}
+
+export interface RetryRequest {
+    isOpenRetry: boolean;
+    count: number;
+    waitTime: number;
+}
