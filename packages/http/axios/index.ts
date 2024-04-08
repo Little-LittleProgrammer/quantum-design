fff7ca169c4cb242262403d701deafd23d042e13
EvanWu <850123119@qq.com>
2024-04-08 17:52:34 +0800

    fix: 修复缺陷

--- packages/http/axios/index.ts
+++ packages/http/axios/index.ts
@@ -5,29 +5,53 @@ import type { AxiosRequestConfig, AxiosResponse } from 'axios';
 import type { AxiosTransform, CreateAxiosOptions } from './axios-transform';
 import { VAxios } from './axios';
 import { check_status } from './check-status';
-import { gContentTypeEnum, gRequestEnum, gResultEnum } from '@quantum-design/shared/enums';
-import { js_utils_deep_merge, js_is_string } from '@quantum-design/utils';
-import { joinTimestamp, joinEnvToUrl, joinCookieToUrl, dealToken } from './helper';
+import {
+    gContentTypeEnum,
+    gRequestEnum,
+    gResultEnum
+} from '@quantum-design/shared/enums';
+import {
+    js_utils_deep_merge,
+    js_is_string,
+    js_is_server
+} from '@quantum-design/utils';
+import {
+    joinTimestamp,
+    joinEnvToUrl,
+    joinCookieToUrl,
+    dealToken
+} from './helper';
 import { AxiosRetry } from './axios-retry';
 
-const {setTokenToHeader, setTokenToLs} = dealToken();
+const { setTokenToHeader, setTokenToLs, } = dealToken();
 
 /**
  * @description: 数据处理，方便区分多种处理方式
  */
 export const defaultTransform: AxiosTransform = {
-
     /**
-     * 发送请求前
-     * @param config
-     * @param options
-     */
+	 * 发送请求前
+	 * @param config
+	 * @param options
+	 */
     beforeRequestHook: (config, options) => {
-        const { apiUrl, urlPrefix, joinTime = true, env = () => '', joinPrefix, joinCookie = true } = options;
+        const {
+            apiUrl,
+            urlPrefix,
+            joinTime = true,
+            env = () => '',
+            joinPrefix,
+            joinCookie = true,
+        } = options;
         const params = config.params || {};
         const data = config.data || false;
 
-        if (!(config.url?.match(/^http/) != null || config.url?.match(/^\/\//) != null)){
+        if (
+            !(
+                config.url?.match(/^http/) != null ||
+				config.url?.match(/^\/\//) != null
+            )
+        ) {
             if (joinPrefix) {
                 config.url = `${urlPrefix}${config.url}`;
             }
@@ -40,26 +64,57 @@ export const defaultTransform: AxiosTransform = {
         if (config.method?.toUpperCase() === gRequestEnum.GET) {
             if (!js_is_string(params)) {
                 // 给 get 请求加上时间戳参数，避免从缓存中拿数据。
-                config.params = Object.assign(params || {}, joinEnvToUrl(env, false), joinTimestamp(joinTime, false), joinCookieToUrl(joinCookie, false));
+                config.params = Object.assign(
+                    params || {},
+                    joinEnvToUrl(env, false),
+                    joinTimestamp(joinTime, false),
+                    joinCookieToUrl(joinCookie, false)
+                );
             } else {
                 // 兼容restful风格
-                config.url = config.url + params + '?' + `${joinTimestamp(joinTime, true)}&` + `${joinEnvToUrl(env, true)}&` + `${joinCookieToUrl(joinCookie, true)}`;
+                config.url =
+					config.url +
+					params +
+					'?' +
+					`${joinTimestamp(joinTime, true)}&` +
+					`${joinEnvToUrl(env, true)}&` +
+					`${joinCookieToUrl(joinCookie, true)}`;
                 config.params = undefined;
             }
         } else {
             if (!js_is_string(params)) {
-                config.url = config.url + '?' + `${joinTimestamp(joinTime, true)}&` + `${joinEnvToUrl(env, true)}&` + `${joinCookieToUrl(joinCookie, true)}`;
-                if (Reflect.has(config, 'data') && config.data && Object.keys(config.data).length > 0) {
-                    config.data = Object.assign(data || {}, joinEnvToUrl(env, false));
+                config.url =
+					config.url +
+					(config.url?.includes('?') ? '&' : '?') +
+					`${joinTimestamp(joinTime, true)}&` +
+					`${joinEnvToUrl(env, true)}&` +
+					`${joinCookieToUrl(joinCookie, true)}`;
+                if (
+                    Reflect.has(config, 'data') &&
+					config.data &&
+					Object.keys(config.data).length > 0
+                ) {
+                    config.data = Object.assign(
+                        data || {},
+                        joinEnvToUrl(env, false)
+                    );
                     config.params = params;
                 } else {
                     // 非GET请求如果没有提供data，则将params视为data
-                    config.data = Object.assign(params || {}, joinEnvToUrl(env, false));
+                    config.data = Object.assign(
+                        params || {},
+                        joinEnvToUrl(env, false)
+                    );
                     config.params = undefined;
                 }
             } else {
                 // 兼容restful风格
-                config.url = config.url + params + '?' + `${joinTimestamp(joinTime, true)}&` + `${joinCookieToUrl(joinCookie, true)}`;
+                config.url =
+					config.url +
+					params +
+					'?' +
+					`${joinTimestamp(joinTime, true)}&` +
+					`${joinCookieToUrl(joinCookie, true)}`;
                 config.params = undefined;
                 config.data = joinEnvToUrl(env, true);
             }
@@ -67,57 +122,129 @@ export const defaultTransform: AxiosTransform = {
         return config;
     },
     /**
-   * @description: 请求拦截器处理
-   */
+	 * @description: 请求拦截器处理
+	 */
     requestInterceptors: (config, options) => {
         if (options.customTransform && options.customTransform.customRequest) {
-            config = options.customTransform.customRequest(config) as SelectPartial<AxiosRequestConfig<any>, 'url' | 'headers' | 'method'>;
+            config = options.customTransform.customRequest(
+                config
+            ) as SelectPartial<
+            AxiosRequestConfig<any>,
+            'url' | 'headers' | 'method'
+            >;
         }
-        config = setTokenToHeader(options, config) as SelectPartial<AxiosRequestConfig<any>, 'url' | 'headers' | 'method'>;
+        config = setTokenToHeader(options, config) as SelectPartial<
+        AxiosRequestConfig<any>,
+        'url' | 'headers' | 'method'
+        >;
         return config;
     },
 
     /**
-     * @description: 请求拦截器错误处理
-     * @param error
-     */
-    requestInterceptorsCatch: (error: any, options, axiosInstance) => {
-        if (options.customTransform && options.customTransform.customRequestError) {
+	 * @description: 请求拦截器错误处理
+	 * @param error
+	 */
+    requestInterceptorsCatch: (error: any, options) => {
+        if (
+            options.customTransform &&
+			options.customTransform.customRequestError
+        ) {
             options.customTransform.customRequestError(error);
         }
         if (error && error.response) {
-            check_status(error.response?.status, '连接错误', error.config?.requestOptions?.errorMessageCb);
+            check_status(
+                error.response?.status,
+                '连接错误',
+                error.config?.requestOptions?.errorMessageCb
+            );
         } else {
-            check_status('400', '连接到服务器失败', error.config?.requestOptions?.errorMessageCb);
-        }
-        // 添加自动重试机制
-        const retry = new AxiosRetry();
-        const {isOpenRetry} = options.requestOptions.retryRequest;
-        if (error && error.config?.method && error.config?.method?.toUpperCase() === gRequestEnum.GET) {
-            isOpenRetry && retry.retry(axiosInstance, error)
+            check_status(
+                '400',
+                '连接到服务器失败',
+                error.config?.requestOptions?.errorMessageCb
+            );
         }
         return Promise.reject(error.response);
     },
 
     /**
-   * @description: 响应拦截器处理
-   */
+	 * @description: 响应拦截器处理
+	 */
     responseInterceptors: (res: AxiosResponse<any>, options) => {
         console.log(res, options);
         if (options.customTransform && options.customTransform.customResponse) {
             res = options.customTransform.customResponse(res);
         }
-        if (res.data.code == gResultEnum.NOTFOUND){
-            location.replace('/backend/error');
-        } else if (res.data.code == gResultEnum.ERROR){
-            check_status('400', res.data.msg, options.requestOptions?.errorMessageCb);
-        } else if (res.data.code == gResultEnum.SERVERERROR){
-            check_status('400', res.data.msg, options.requestOptions?.errorMessageCb);
-        } else if (res.data.code == gResultEnum.RELOAD){
+        setTokenToLs(options.requestOptions?.withToken || true, res);
+        if (res.data.code == gResultEnum.RELOAD) {
             setTokenToLs(options.requestOptions?.withToken || true, res);
-            window.location.reload();
-        } else if (res.data.code == gResultEnum.LOGIN){
-            window.location.href = res.data.data?.url;
+            if (js_is_server) {
+                // node环境
+                if (process.env?.NODE_ENV == 'production') {
+                    window.location.reload();
+                } else {
+                    console.warn(
+                        '请复制测试环境的token与cookie到本地环境, 或设置本地nginx来代理localhost'
+                    );
+                }
+            } else if (import.meta && (import.meta as any).env) {
+                // vite 浏览器
+                if (!(import.meta as any).env.DEV) {
+                    window.location.reload();
+                } else {
+                    console.warn(
+                        '请复制测试环境的token与cookie到本地环境, 或设置本地nginx来代理localhost'
+                    );
+                }
+            } else {
+                window.location.reload();
+            }
+            return res;
+        } else if (res.data.code == gResultEnum.LOGIN) {
+            if (js_is_server) {
+                // node环境
+                if (process.env?.NODE_ENV == 'production') {
+                    window.location.href = res.data.data?.url;
+                } else {
+                    console.warn(
+                        '请复制测试环境的token与cookie到本地环境, 或设置本地nginx来代理localhost'
+                    );
+                }
+            } else if (import.meta && (import.meta as any).env) {
+                // vite 浏览器
+                if (!(import.meta as any).env.DEV) {
+                    window.location.href = res.data.data?.url;
+                } else {
+                    console.warn(
+                        '请复制测试环境的token与cookie到本地环境, 或设置本地nginx来代理localhost'
+                    );
+                }
+            } else {
+                window.location.href = res.data.data?.url;
+            }
+            return res;
+        } else if (res.data.code == gResultEnum.NOTFOUND) {
+            location.replace('/backend/error');
+            return res;
+        }
+        if ((res.config as any).requestOptions?.useServiceMsg && res.data.code != gResultEnum.SUCCESS) {
+            check_status(
+                '400',
+                res.data.msg,
+                options.requestOptions?.errorMessageCb
+            );
+        } else if (res.data.code == gResultEnum.ERROR) {
+            check_status(
+                '400',
+                res.data.msg,
+                options.requestOptions?.errorMessageCb
+            );
+        } else if (res.data.code == gResultEnum.SERVERERROR) {
+            check_status(
+                '400',
+                res.data.msg,
+                options.requestOptions?.errorMessageCb
+            );
         } else if (res.data.code == gResultEnum.TIMEOUT) {
             check_status('408', '', options.requestOptions?.errorMessageCb);
         }
@@ -125,27 +252,52 @@ export const defaultTransform: AxiosTransform = {
     },
 
     /**
-   * @description: 响应错误处理
-   */
-    responseInterceptorsCatch: (error: any, options) => {
+	 * @description: 响应错误处理
+	 */
+    responseInterceptorsCatch: (error: any, options, axiosInstance) => {
         console.log(error, options);
-        if (options.customTransform && options.customTransform.customResponseError) {
+        if (
+            options.customTransform &&
+			options.customTransform.customResponseError
+        ) {
             options.customTransform.customResponseError(error);
         }
         if (error && error.response) {
-            check_status(error.response?.status, '连接错误', options.requestOptions?.errorMessageCb);
+            check_status(
+                error.response?.status,
+                '连接错误',
+                options.requestOptions?.errorMessageCb
+            );
         } else if (error.code === 'ERR_CANCELED') {
             // 如果手动取消, 不予受理
             console.log('请求重复, 手动取消请求');
             return Promise.resolve(error);
         } else {
-            check_status('400', '连接到服务器失败', options.requestOptions?.errorMessageCb);
+            check_status(
+                '400',
+                '连接到服务器失败',
+                options.requestOptions?.errorMessageCb
+            );
+        }
+
+        // 添加自动重试机制
+        const _retry = new AxiosRetry();
+        const { isOpenRetry, } = options.requestOptions?.retryRequest || {};
+        // 重试请求只针对get
+        if (
+            error &&
+			error.config?.method &&
+			error.config?.method?.toUpperCase() === gRequestEnum.GET
+        ) {
+            isOpenRetry && _retry.retry(axiosInstance, error);
         }
         return Promise.reject(error.response ? error.response : error);
-    }
+    },
 };
 
-export function createAxios(opt?: Omit<Partial<CreateAxiosOptions>, 'defaultTransform'>) {
+export function createAxios(
+    opt?: Omit<Partial<CreateAxiosOptions>, 'defaultTransform'>
+) {
     return new VAxios(
         js_utils_deep_merge(
             {
@@ -154,7 +306,7 @@ export function createAxios(opt?: Omit<Partial<CreateAxiosOptions>, 'defaultTran
                 // authenticationScheme: 'Bearer',
                 authenticationScheme: '',
                 timeout: 60 * 1000,
-                headers: { 'Content-Type': gContentTypeEnum.FORM_URLENCODED },
+                headers: { 'Content-Type': gContentTypeEnum.FORM_URLENCODED, },
                 defaultTransform,
                 customTransform: {},
                 // 配置项，下面的选项都可以在独立的接口请求中覆盖
@@ -177,8 +329,14 @@ export function createAxios(opt?: Omit<Partial<CreateAxiosOptions>, 'defaultTran
                     apiUrl: '',
                     uploadUrl: '',
                     // 接口拼接地址
-                    urlPrefix: 'api'
-                }
+                    urlPrefix: 'api',
+                    // 重试机制
+                    retryRequest: {
+                        isOpenRetry: false,
+                        count: 5,
+                        waitTime: 100,
+                    },
+                },
             },
             opt || {}
         )
