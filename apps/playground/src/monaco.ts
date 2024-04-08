8f75b4037de1db9c5a8fea41a8c54f5768394df4
EvanWu <850123119@qq.com>
2024-04-08 17:39:16 +0800

    fix: test

--- apps/playground/src/monaco.ts
+++ apps/playground/src/monaco.ts
@@ -22,7 +22,7 @@ export function install_monaco() {
                 return new TsWorker();
             }
             return new EditorWorker();
-        },
+        }
     };
 
     monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
