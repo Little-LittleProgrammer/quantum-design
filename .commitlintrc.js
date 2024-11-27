0da81e35c2e0059809d5712cf1849fd47dc33a34
EvanWu <850123119@qq.com>
2024-11-27 13:29:26 +0800

    chore: 增加husky, lint等教研

---
+++ .commitlintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+    extends: ['@q-front-npm-configs/commitlint']
+}
---
+++ .lintstagedrc.mjs
@@ -0,0 +1,18 @@
+export default {
+  '*.{js,jsx,ts,tsx}': [
+    // 'prettier --cache --ignore-unknown  --write',
+    'eslint --cache --fix',
+  ],
+  '*.{scss,less,styl,html,vue,css}': [
+    'prettier --cache --ignore-unknown --write',
+  ],
+  '*.md': ['prettier --cache --ignore-unknown --write'],
+  '*.vue': [
+    // 'prettier --write',
+    'eslint --cache --fix',
+  ],
+  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
+    'prettier --cache --write--parser json',
+  ],
+  'package.json': ['prettier --cache --write'],
+};
