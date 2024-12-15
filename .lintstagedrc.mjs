3a35afc4169df746e7de59155b6e7cc07acf1d2c
EvanWu <850123119@qq.com>
2024-12-15 12:35:56 +0800

    chore: 更新依赖

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,18 +1,19 @@
 export default {
-  '*.{js,jsx,ts,tsx}': [
-    // 'prettier --cache --ignore-unknown  --write',
-    'eslint --cache --fix',
-  ],
-  '*.{scss,less,styl,html,vue,css}': [
-    'prettier --cache --ignore-unknown --write',
-  ],
-  '*.md': ['prettier --cache --ignore-unknown --write'],
-  '*.vue': [
-    // 'prettier --write',
-    'eslint --cache --fix',
-  ],
-  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
-    'prettier --cache --write--parser json',
-  ],
-  'package.json': ['prettier --cache --write'],
-};
+    '*.{js,jsx,ts,tsx}': [
+      // 'prettier --cache --ignore-unknown  --write',
+      'eslint --cache --fix',
+    ],
+    '*.{scss,less,styl,html,vue,css}': [
+      'prettier --cache --ignore-unknown --write',
+    ],
+    '*.md': ['prettier --cache --ignore-unknown --write'],
+    '*.vue': [
+      // 'prettier --write',
+      'eslint --cache --fix',
+    ],
+    '{!(package)*.json,*.code-snippets,.!(browserslist|npm)*rc}': [
+      'prettier --cache --write--parser json',
+    ],
+    'package.json': ['prettier --cache --write'],
+  };
+  
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    printWidth: 500, // 此长度跟eslint  'max-len': [2, 500], 保持一致
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'always',
+};
