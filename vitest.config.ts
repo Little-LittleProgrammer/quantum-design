3ed2875645d7adaecd70ad1a2f86c4c001420e3c
EvanWu <850123119@qq.com>
2025-08-21 14:10:39 +0800

    feat: 更新 Vite 配置和依赖项
    - 在 package.json 中添加 @vitejs/plugin-vue 和 @vitejs/plugin-vue-jsx 依赖
    - 更新 pnpm-lock.yaml，移除不再使用的 @vitest/coverage-v8 依赖
    - 修改 vitest.config.ts，添加 Vue 和 Vue JSX 插件，更新测试环境为 happy-dom
    - 删除不再需要的 vitest.workspace.ts 文件
    - 更新 configs/vite/package.json，调整 peerDependencies 和 dependencies 的配置
    - 在 vue3-pc-ui 的 package.json 中移除 @vue/test-utils 依赖

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vitest/config';
+import Vue from '@vitejs/plugin-vue';
+import VueJsx from '@vitejs/plugin-vue-jsx';
+import { configDefaults, defineConfig } from 'vitest/config';
+
 export default defineConfig({
+    plugins: [Vue(), VueJsx()],
     test: {
-        environment: 'jsdom'
-    }
+        environment: 'happy-dom',
+        exclude: [...configDefaults.exclude, '**/e2e/**'],
+        projects: ['packages/*'],
+        reporters: ['html'],
+    },
 });
